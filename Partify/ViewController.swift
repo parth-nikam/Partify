2d8c7bf9a345f197e540f8cc45e8cbbb6eca9d80
parth-nikam <parthsnikam12co@gmail.com>
2022-12-15 16:20:34 +0530

    Initial Commit

---
+++ Partify/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Partify
+//
+//  Created by Parth Nikam on 15/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
