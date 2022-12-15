4594a1961971f632f8592a783dc8f4ea6b81d6d6
parth-nikam <parthsnikam12co@gmail.com>
2022-12-15 17:59:05 +0530

    First version of the app. Plays music when we click the first button names "One"

--- Partify/ViewController.swift
+++ Partify/ViewController.swift
@@ -6,12 +6,45 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var player: AVAudioPlayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func keyPressed(_ sender: UIButton) {
+        playSound()
+    }
+    
+    func playSound() {
+        if let player = player, player.isPlaying{
+            player.stop()
+             
+        }
+        else{
+            let urlString = Bundle.main.path(forResource: "One", ofType: "mp3")
+            do {
+               try AVAudioSession.sharedInstance().setMode(.default)
+               try AVAudioSession.sharedInstance().setActive(true, options: .notifyOthersOnDeactivation)
+                guard let urlString = urlString else{
+                    return
+                }
+                player = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: urlString))
+                guard let player = player else{
+                    return
+                }
+                
+                player.play()
+                
+            } catch  {
+                print("something went wrong")
+            }
+        }
     }
 
 
